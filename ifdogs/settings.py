d2dc5f8569053b86ac5488110747bf4bb03005f2
gustavorteuber <gugareis135@gmail.com>
2022-10-13 11:04:30 -0300

    projeto-com-docker

--- ifdogs/settings.py
+++ ifdogs/settings.py
@@ -89,6 +89,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ifdog',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:8000' , 'http://127.0.0.1:8000']
