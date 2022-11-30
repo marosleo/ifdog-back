25d726568cefe5794e8a0830e9a0642fc8e1e523
marosleo <marosleo1990@gmail.com>
2022-11-30 14:04:34 -0300

    campo "publicado" tirado

--- core/models/Comentarios.py
+++ core/models/Comentarios.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 class Comentarios(models.Model):
     texto = models.TextField()
     autor  = models.ForeignKey("core.Usuario",on_delete=models.CASCADE)
-    publicado = publicado = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.texto
