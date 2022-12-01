aaacb37ba4267027cb02d5d611cdee6323e3c910
gustavorteuber <gugareis135@gmail.com>
2022-12-01 16:05:25 -0300

    Banco atualizado

--- core/models/Cachorro.py
+++ core/models/Cachorro.py
@@ -1,7 +1,8 @@
 from django.db import models
 from media.models import Image
 
-class Cachorro(models.Model): #cards dos cachorros
+
+class Cachorro(models.Model):  # cards dos cachorros
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     foto = models.ForeignKey(
@@ -14,10 +15,9 @@ class Cachorro(models.Model): #cards dos cachorros
     )
     peso = models.DecimalField(max_digits=8, decimal_places=2)
     altura = models.DecimalField(max_digits=8, decimal_places=2)
-    nome_responsavel = models.CharField(max_length=100, default='')
-    tel_responsavel = models.CharField(default='', max_length=100)
+    nome_responsavel = models.CharField(max_length=100, default="")
+    tel_responsavel = models.CharField(default="", max_length=100)
     castrado = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.nome
-  
