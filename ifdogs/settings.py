7110882478cddd02e07cacc74c2c17fe004af737
gustavorteuber <gugareis135@gmail.com>
2022-12-08 10:58:01 -0300

    imagens

--- ifdogs/settings.py
+++ ifdogs/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'ifdogs.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:8000' , 'http://127.0.0.1:8000', 'https://horseti.pythonanywhere.com/']
+CORS_ALLOWED_ORIGINS = ['https://horseti.pythonanywhere.com/']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
