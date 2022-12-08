5a9ac83f15fc0300b68e698eef0ad1857098fb3b
brunahenz <brunaehenz@gmail.com>
2022-12-08 08:34:34 -0300

    python anywhere

--- ifdogs/settings.py
+++ ifdogs/settings.py
@@ -9,27 +9,22 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# SECRET_KEY = 'django-insecure-+kzcue519&9zc6&$tex#e4198048s@&@hb6j3fi%vtk#cqo8d6'
+# DEBUG = True
+# ALLOWED_HOSTS = ['*']
 
+env = environ.Env()
+environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+kzcue519&9zc6&$tex#e4198048s@&@hb6j3fi%vtk#cqo8d6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-
-# Application definition
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+DATABASES = {'default': env.db()}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -82,12 +77,12 @@ WSGI_APPLICATION = 'ifdogs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.sqlite3",
+    #         "NAME": BASE_DIR / "db.sqlite3",
+    #     }
+    # }
 
 # DATABASES = {
 #      'default': {
